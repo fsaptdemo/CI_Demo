ef387854eaa811c5d990f316be54b81e7af812f1
Jessica Duell <duellsoftwaredev@gmail.com>
2023-07-20 18:57:44 -0400

    added test

--- server/tests/routes.spec.js
+++ server/tests/routes.spec.js
@@ -2,6 +2,11 @@
  * STEP 1
  * Create a basic test to pass in the test folder
  */
+const { expect } = require("chai");
+
+it("true is true", () => {
+  expect(true).to.be(true);
+});
 
 /**
  * STEP 5
